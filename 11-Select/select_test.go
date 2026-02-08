beccdef10648e2c41e2c20bb4f89bb9f59ed727b
Fernando Carvalho <fernanduzz@gmail.com>
2026-02-08 12:43:59 +0000

    Add tests for Racer and ConfigurableRacer functions with delayed server simulation

---
+++ 11-Select/select_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	t.Run("compares speeds of two servers", func(t *testing.T) {
+
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if want != got {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Returns an error if server doesn't respon within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 1*time.Nanosecond)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+
+	})
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
