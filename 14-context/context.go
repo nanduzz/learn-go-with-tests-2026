3b56f12e2893d867162fbf0a41a83d6bec970e1b
Fernando Carvalho <fernanduzz@gmail.com>
2026-02-10 10:30:58 +0000

    Add Server implementation and tests for context-aware data fetching

---
+++ 14-context/context.go
@@ -0,0 +1,26 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			log.Printf("failed to fetch data: %v", err)
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
+
+}
